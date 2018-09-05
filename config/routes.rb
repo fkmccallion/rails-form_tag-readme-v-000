1601ba1fd160ad70f4caeffeb4a0caf74e552004
Frank McCallion <fkmccallion@gmail.com>
2018-09-05 12:33:36 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   # Adding comments below to refer back to original code
 
   #resources :posts, only: [:index]
-  resources :posts, only: [:index, :new]
+  resources :posts, only: [:index, :new, :create]
 end
